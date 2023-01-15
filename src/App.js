ec80ce4fc19809ade5f629d4773efcc3ccf82531
dipankar121 <94380559+dipankar121@users.noreply.github.com>
2023-01-15 12:40:45 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
+import Home from './Home';
+import ProductPage from './components/ProductPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-gray-100 min-h-screen">
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/:productID" element={<ProductPage />} />
+      </Routes>
+      <Cart />
     </div>
   );
 }
